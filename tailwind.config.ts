4c84d6e8aff8efc9a4820a91ecf3ce79acf9c5b6
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-03-10 22:56:58 -0300

    🔧 setup tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,26 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          500: '#14b8a6',
+          800: '#083344',
+          900: '#070B15',
+        },
+        secondary: {
+          100: '#fafafa',
+          200: '#E3E7EE',
+          800: '#475569',
+          900: '#182138',
+        },
+        success: {
+          700: '#1d7561',
+        },
+        warning: {
+          700: '#ea580c',
+        },
+        'gradient-primary': 'linear-gradient(127deg, #F10808 9.24%, #6B90F4 109.33%)',
+      },
     },
   },
   plugins: [],
