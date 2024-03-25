b5890d01d82f4baa07e1f4147450b52f21e4f7cb
Jonathan Max Saravia Moreira <saravia.jonathan.m@gmail.com>
2024-03-24 23:43:11 -0300

    💄 update margin bottom

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 function Dashboard() {
   return (
     <main className="flex grow flex-col items-center justify-center">
-      <p className="mb-6 text-4xl">Dashboard</p>
+      <p className="mb-5 text-4xl">Dashboard</p>
     </main>
   );
 }
